9f1119ab52dc956180c4c8f250a65eb3d91f21c1
Keith Sharp <kms@passback.co.uk>
2022-09-11 14:26:04 +0100

    Simple Clap arg parsing.

---
+++ src/bin/clap-basic-args.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Arguments {
+    name: String,
+    #[clap(short, long, action)]
+    debug: bool,
+}
+
+fn main() {
+    let args = Arguments::parse();
+    
+    if args.debug {
+        println!("{:?}", args);
+    }
+    println!("Hello, {}", args.name);
+}
