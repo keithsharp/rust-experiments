09bf8a7cd6beca7a9c6c476ad3a18dca1fcd66fe
Keith Sharp <kms@passback.co.uk>
2022-10-17 14:22:19 +0100

    Filter list of VPCs based on tag names and values.

---
+++ src/bin/vpc-filter.rs
@@ -0,0 +1,48 @@
+use std::env;
+
+use aws_config::default_provider::region::DefaultRegionChain;
+use aws_config::default_provider::credentials::DefaultCredentialsChain;
+
+use aws_sdk_ec2::Error;
+use aws_sdk_ec2::Client;
+use aws_sdk_ec2::Region;
+
+use aws_sdk_ec2::model::Filter;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let profile = env::args().skip(1).next().expect("profile to use is required");
+
+    let region = DefaultRegionChain::builder()
+        .profile_name(&profile)
+        .build()
+        .region()
+        .await
+        .or(Some(Region::new("eu-west-1")));
+
+    let creds = DefaultCredentialsChain::builder()
+        .profile_name(&profile)
+        .region(region.clone())
+        .build()
+        .await;
+
+    let config = aws_config::from_env()
+        .credentials_provider(creds)
+        .region(region)
+        .load()
+        .await;
+
+    let client = Client::new(&config);
+
+    let filter = Filter::builder()
+        .name("tag:Name")
+        .values("test-soto")
+        .build();
+
+    let req = client.describe_vpcs()
+        .filters(filter);
+    let resp = req.send().await?;
+    println!("{:?}", resp);
+
+    Ok(())
+}
