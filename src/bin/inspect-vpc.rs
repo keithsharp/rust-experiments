3a3ac0fec9b97f8b2f5850d762af4c474e888c2b
Keith Sharp <kms@passback.co.uk>
2023-03-05 10:42:04 +0000

    WIP: slight rework to reduce calls to AWS.

--- src/bin/inspect-vpc.rs
+++ src/bin/inspect-vpc.rs
@@ -1,5 +1,8 @@
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_ec2::{model::Filter, Client};
+use aws_sdk_ec2::{
+    model::{Filter, Vpc},
+    Client,
+};
 
 use anyhow::anyhow;
 
@@ -12,26 +15,58 @@ async fn main() -> anyhow::Result<()> {
     let config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&config);
 
-    let vpcid = match std::env::args().nth(1) {
-        Some(vpcid) => vpcid,
-        _ => get_default_vpc_id(&client).await?,
+    let vpc = match std::env::args().nth(1) {
+        Some(vpcid) => get_vpc(&client, &vpcid).await?,
+        _ => get_default_vpc(&client).await?,
     };
-    println!("{vpcid}");
+
+    print_vpc_info(&vpc);
 
     Ok(())
 }
 
-async fn get_default_vpc_id(client: &Client) -> anyhow::Result<String> {
+async fn get_vpc(client: &Client, vpcid: &str) -> anyhow::Result<Vpc> {
+    let filter = Filter::builder().name("vpc-id").values(vpcid).build();
+
+    let resp = client.describe_vpcs().filters(filter).send().await?;
+
+    if let Some(vpcs) = resp.vpcs() {
+        if let Some(vpc) = vpcs.get(0) {
+            return Ok(vpc.clone());
+        }
+    }
+
+    Err(anyhow!(format!("Could not find VPC with ID: {}", &vpcid)))
+}
+
+async fn get_default_vpc(client: &Client) -> anyhow::Result<Vpc> {
     let filter = Filter::builder().name("is-default").values("true").build();
 
     let resp = client.describe_vpcs().filters(filter).send().await?;
 
     if let Some(vpcs) = resp.vpcs() {
         if let Some(vpc) = vpcs.get(0) {
-            let vpcid = vpc.vpc_id().expect("should always get a VPC ID");
-            return Ok(vpcid.to_owned());
+            return Ok(vpc.clone());
         }
     }
 
     Err(anyhow!("Could not find a default VPC"))
+}
+
+fn print_vpc_info(vpc: &Vpc) {
+    let vpcid = vpc.vpc_id().expect("a VPC should always have an ID");
+    print!("VPC ID: {vpcid}");
+    if vpc
+        .is_default()
+        .expect("a VPC should always have a flag for is_default")
+    {
+        println!(" (default)");
+    } else {
+        println!();
+    }
+
+    let accountid = vpc
+        .owner_id()
+        .expect("a VPC should always have an owner_id");
+    println!("    Account ID: {accountid}");
 }
